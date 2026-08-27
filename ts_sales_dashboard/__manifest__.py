1cbd6b11cded4a7d9be786d6a943b6da1a8150e5
selimovich <mirsadselimovich@gmail.com>
2026-08-28 01:13:51 +0300

    update_company

--- ts_sales_dashboard/__manifest__.py
+++ ts_sales_dashboard/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Advanced Sales Dashboard",
     "summary": "Executive sales dashboard with revenue, customers, products, geography, and team KPIs. Works on Odoo Community and Enterprise.",
-    "version": "19.0.2.0.0",
+    "version": "19.0.2.2.0",
     "category": "Sales",
     "author": "Mudassir Amin, Inovado D.o.o., Mirsad Selimovic, Techman Solutions",
     "website": "https://inovado.ba, https://techman.qa, https://www.mudassir.it.com",
