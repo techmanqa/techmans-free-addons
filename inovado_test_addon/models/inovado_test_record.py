ea71cf9034ce38d4c6b1dc04a9c9552050d62d50
selimovich <mirsadselimovich@gmail.com>
2026-09-20 23:20:28 +0200

    deleteall

--- inovado_test_addon/models/inovado_test_record.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class InovadoTestRecord(models.Model):
-    _name = "inovado.test.record"
-    _description = "Inovado Test Record"
-
-    name = fields.Char(required=True)
